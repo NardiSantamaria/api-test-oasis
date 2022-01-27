de857740a37b0bc384213f79244c34e4cedab66d
SantamariaNardi <nardisuribel@gmail.com>
2022-01-27 10:12:18 -0500

    api

--- app/Http/Controllers/getController.php
+++ app/Http/Controllers/getController.php
@@ -26,7 +26,7 @@ class getController extends Controller
         //dd($horarios);
         $bares= CentrosConsumo::join('centros_consumo_detalles', 'centros_consumo.id', '=', 'centros_consumo_detalles.centro_consumo_id')->where('centros_consumo_detalles.hotel_id', '1')->where('categoria_id', '3')->get();
         $restaurants = CentrosConsumo::join('centros_consumo_detalles', 'centros_consumo.id', '=', 'centros_consumo_detalles.centro_consumo_id')->where('centros_consumo_detalles.hotel_id', '1')->where('categoria_id', '2')->get();
-        
+    
         //dd($categorias, $centros_consumo, $centros_consumo_detalles, $horarios);
         return response()->json(['categorias' => $categorias, 'detalles' => $centros_consumo_detalles, 'horarios' => $horarios, 'bares'=> $bares, 'restaurantes'=>$restaurants]);
     }
